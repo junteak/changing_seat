67dce41ae769f2aa41abf05ff12b859261250a1e
juntanaka <edamame.zombie@gmail.com>
2019-11-05 13:25:58 +0900

    シンプル化

---
+++ changing_seat.py
@@ -0,0 +1,22 @@
+import random
+
+
+# 空のseat_noと、join_memberの配列をあらかじめ作る
+def changing_seat(join_members):
+    print('2019年 Python編\n\n')
+
+    random.shuffle(join_members)
+
+    print('---右側の席(1~4)---')
+    for i in range(0, 4):
+        print(f'{i + 1}. {join_members[i]}')
+    print('')
+
+    print('---左側の席(5~8)---')
+    for i in range(4, 8):
+        print(f'{i + 1}. {join_members[i]}')
+    print('')
+
+    print('---左側の席(9~14)---')
+    for i in range(8, 14):
+        print(f'{i + 1}. {join_members[i]}')
